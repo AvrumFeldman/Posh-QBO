0a36839b703b236109d004ca22e29fd847719b1d
Avrumi Feldman (laptop) <avsfeld@gmail.com>
2021-02-10 01:37:32 -0500

    Rewritten Parse-Chase using new PDF parser

--- parse-chase.ps1
+++ parse-chase.ps1
@@ -1,50 +1,61 @@
-Add-Type -Path "$PSScriptRoot\dlls\itext.kernel.dll"
+param(
+    [CmdletBinding()]
+    $path
+)
 
-# Regex options
-$m = [System.Text.RegularExpressions.RegexOptions]::Multiline
-$s = [System.Text.RegularExpressions.RegexOptions]::Singleline
+# Parse the document using PdfPig
+Add-Type -Path "$PSScriptRoot\dlls\UglyToad.PdfPig.dll"
+$document = [UglyToad.PdfPig.PdfDocument]::Open($path)
 
-# Convert PDF to text
-$path = "C:\Users\Feldmans\Downloads\20141128-statements-3099- (1).pdf"
-$pdfDoc  = [iText.Kernel.Pdf.PdfDocument]::new([iText.Kernel.Pdf.PdfReader]::new($path))
-$text = for ($a = 1; $a -le $pdfdoc.GetNumberOfPages(); $a++) {
-    [iText.Kernel.Pdf.Canvas.Parser.PdfTextExtractor]::GetTextFromPage($pdfDoc.GetPage($a))
+# Extract rows from PDF.
+$words = for ($i = 1; $i -lt $document.NumberOfPages; $i++) {
+    # Sort the rows page by page, otherwisw thw rows from all pages get mixed up, as they use coordinates per page. 
+    $document.GetPage($i).getwords() | Group-Object {$_.BoundingBox.bottom} | Sort-Object -Descending {[double]$_.name}
 }
-$text = $text -join "`n"
+# Flatten text output to single string object. Needed to be able to easily split by account number.
+$text = ($words | ForEach-Object {$_.group -join " "}) -join "`n"
 
-# For newer accessible PDF's (From circa Nov 2016) there is some markers we can use to break up the document based on account and extract the transactions.
-$regex = [regex]::Matches($text, "^\*start\*globalproduct$.*?^\*start\*posttransaction detail message$", @($s,$m))
-
-# Confirm the document is indeed a newer one, otherwise this will be empty.
-if ([string]::IsNullOrWhiteSpace($regex)) {
-    # In order not to need to have seperate code for older and newer statements, we are imitating the regex object so the code will work on both the same.
-    $regex = [pscustomobject]@{value = $text}
-}
+# Split the document by accounts
+$regex = [pscustomobject]@{value = ($text -split "Account number") | Where-Object {$_[0] -eq ":"}}
     
 # Get the dates the statement is refering to. It is important as the dates on the transactions is missing the year so we are using the year from this date to fix that.
-$dates = [regex]::Match($text,".*[0-9]{2}, [0-9]{4} through .* [0-9]{2}, [0-9]{4}").value -split "through" | foreach {$_ | get-date}
+$dates = [regex]::Match($text,"\w+ [0-9]{2}, [0-9]{4} through ?\w+ [0-9]{2}, [0-9]{4}").value -split "through" | foreach-object {$_ | get-date}
 
 $regex.value | foreach-object {
-    $trans = ($_ -split "`n") | where-object {$_ -match  "[0-9]{2}\/[0-9]{2}"}
-    $transaction = $trans | ForEach-Object {
-        $tr_regex = [regex]::Match($_,"([0-9]+\/[0-9]+) (.*) (-?[0-9]+\,?[0-9]*\.[0-9]+) (-?[0-9]+\,?[0-9]*\.[0-9]+)").Groups[1..4].value
-        
-        # Add the year to the transaction date. 
-        if (($tr_regex[0] -split "/")[0] -lt 12) {
-            $Date = "$($tr_regex[0])/$($dates[1].Year)"
+    $tr_regex = [regex]::Matches($_,"([0-9]+\/[0-9]+) (.*) (-?[0-9]+\,?[0-9]*\.[0-9]+) (-?[0-9]+\,?[0-9]*\.[0-9]+)")
+    
+    $account_number = [regex]::Match($_, "(?<=: )[0-9]+\b").value
+
+    $transaction = $tr_regex | ForEach-Object {
+
+        # Convert short date to full date. Utilizes the year listed in the document statement range
+        if (($_.groups[1].value -split "/")[0] -lt 12) {
+            $date = "$($_.groups[1].value)/$($dates[1].Year)"
         } else {
-            $date = "$($tr_regex[0])/$($dates[0].Year)"
+            $date = "$($_.groups[1].value)/$($dates[0].Year)"
         }
 
+        # Fix where sometimes the PDF parser places a space between the negative symbol and the amount,
+        # and the amount is wrong plus the negative is included in the memo.
+        if (($_.Groups[2].Value)[-1] -eq "-") {
+            $memo       = ($_.Groups[2].Value).TrimEnd(" -")
+            $amount     = "-$($_.Groups[3].Value)"
+        } else {
+            $memo       = $_.Groups[2].Value
+            $amount     = $_.Groups[3].Value
+        }
 
         [pscustomobject]@{
             Date        = $date
-            Memo        = $tr_regex[1]
-            Amount      = $tr_regex[2]
+            Memo        = $memo
+            Amount      = $amount
+            Account     = $account_number
+            file        = [io.path]::GetFileName($path)
         }
     }
     [pscustomobject]@{
-        account = [regex]::Match($_, "(?<=Account Number: ).*?$",@($s,$m)).value
+        account = $account_number
         transactions = $transaction
+        file        = [io.path]::GetFileName($path)
     }
 }
